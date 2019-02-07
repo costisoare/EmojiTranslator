ab73da8a90b5f13fec2421e08901c9797113046c
Constantin Soare <costi.soare@yahoo.com>
2019-02-07 14:13:39 +0000

    Small change to DB tab
    The scrollind does not happen on the entire window, but only on the panel with the emojis.

--- EmojiTranslator/emoji_translator_gui/emoji_db_tab.py
+++ EmojiTranslator/emoji_translator_gui/emoji_db_tab.py
@@ -5,23 +5,22 @@ from emoji_translator_utils.emoji_categ_parser import *
 from emoji_translator_utils.emoji_dict_utils import *
 from emoji_translator_gui.emoji_gui_utils import EmojiBitmap
 
-class EmojiDBTab(ScrolledPanel):
+class EmojiDBTab(wx.Panel):
     def __init__(self, parent):
-        ScrolledPanel.__init__(self, parent)
+        wx.Panel.__init__(self, parent)
         self.SetBackgroundColour((255, 253, 208))
-        self.SetupScrolling(True)
         self.emoji_categories = emoji_categs_from_file()
         self.emoji_categ_buttons = dict()
 
         self.dbtab_sizer = wx.FlexGridSizer(3, 1, 0, 0)
-        self.dbtab_sizer.AddGrowableRow(1)
+        self.dbtab_sizer.AddGrowableRow(2)
         self.dbtab_sizer.AddGrowableCol(0)
 
         self.button_sizer = wx.GridSizer(1, len(self.emoji_categories), 0, 0)
         self.emoji_bmps_panel = wx.Panel()
 
         self.dbtab_sizer.Add(self.button_sizer, 1, wx.EXPAND)
-
+        self.dbtab_sizer.AddSpacer(10)
         for cat in self.emoji_categories:
             bmp_path = unicode_to_filename(STRING_UNICODE[self.emoji_categories[cat][0]], 64)
             self.emoji_categ_buttons[cat] = wx.BitmapToggleButton(self, label=wx.Bitmap(bmp_path),
@@ -30,7 +29,6 @@ class EmojiDBTab(ScrolledPanel):
             self.emoji_categ_buttons[cat].SetBackgroundColour((255, 255, 255))
             self.button_sizer.Add(self.emoji_categ_buttons[cat], 1, wx.EXPAND)
 
-        self.dbtab_sizer.AddSpacer(30)
         self.SetSizer(self.dbtab_sizer)
 
     def OnEmojiCategory(self, event):
@@ -42,7 +40,6 @@ class EmojiDBTab(ScrolledPanel):
                 self.emoji_categ_buttons[cat].SetBackgroundColour((255, 255, 255))
 
         self.populate_grid_with_emojis(pressed_button.GetName())
-        self.SetupScrolling()
         self.Layout()
 
     def populate_grid_with_emojis(self, category):
@@ -50,9 +47,10 @@ class EmojiDBTab(ScrolledPanel):
         self.emoji_bmps_panel = EmojiPanel(self, self.emoji_categories[category])
         self.dbtab_sizer.Add(self.emoji_bmps_panel, 1, wx.EXPAND)
 
-class EmojiPanel(wx.Panel):
+class EmojiPanel(ScrolledPanel):
     def __init__(self, parent, category_list):
-        wx.Panel.__init__(self, parent)
+        ScrolledPanel.__init__(self, parent)
+        self.SetupScrolling()
         self.Show(False)
         self.sizer = wx.GridSizer(len(category_list) / 8 + 1, 8, 15, 0)
         self.emoji_size = 64
