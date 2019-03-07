18511e16c1b32ba2101301a4c5d5677b19f97c84
Constantin Soare <costi.soare@yahoo.com>
2019-03-07 15:58:41 +0000

    remove print

--- EmojiTranslator/emoji_translator_gui/emoji_db_tab.py
+++ EmojiTranslator/emoji_translator_gui/emoji_db_tab.py
@@ -78,7 +78,6 @@ class EmojiDBTab(wx.Panel):
         self.emoji_bmps_panel.Destroy()
         if self.user_profile.username != "guest":
             sorted_most_used = [x[0] for x in sorted(self.user_profile.used_emojis.items(), key=lambda kv: kv[1], reverse=True)]
-            print(sorted_most_used)
             self.emoji_categories["most_used"] = sorted_most_used
         self.emoji_bmps_panel = EmojiPanel(self, category, self.emoji_categories[category], composer=self.composer)
         self.dbtab_sizer.Add(self.emoji_bmps_panel, 1, wx.EXPAND|wx.ALIGN_LEFT)
