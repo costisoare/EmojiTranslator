a0d650fd13876e3c7c8c71a417f285a6616a27ab
Constantin Soare <costi.soare@yahoo.com>
2019-03-19 18:55:42 +0000

    Improved both ways of doing LDA
    - both have same functions, so compatibility is ensured

--- EmojiTranslator/emoji_mining/gensim_LDA.py
+++ EmojiTranslator/emoji_mining/gensim_LDA.py
@@ -2,49 +2,67 @@ import gensim
 import gensim.corpora as corpora
 import pandas
 import re
+import os
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-def sent_to_words(sentences):
+def predict_topic(text, model):
+    words = gensim.utils.simple_preprocess(str(text), deacc=True)
+
+    stop_words = stopwords.words('english')
+    stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+    lemmatizer = WordNetLemmatizer()
+
+    data_lemmatized = list()
+
+    data_lemmatized.append([lemmatizer.lemmatize(w) for w in words if w not in stop_words])
+    id2word = corpora.Dictionary(data_lemmatized)
+    texts = data_lemmatized
+    corpus = [id2word.doc2bow(text) for text in texts]
+
+    unseen_doc = corpus[0]
+    vector = sorted(model[unseen_doc], key=lambda x: x[1], reverse=True)
+    return vector
+
+def create_model(data="20newsgroups"):
+    stop_words = stopwords.words('english')
+    stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+
+    df = pandas.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
+
+    # Convert to list
+    data = df.content.values.tolist()
+    # Remove Emails
+    data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
+    # Remove new line characters
+    data = [re.sub('\s+', ' ', sent) for sent in data]
+    # Remove distracting single quotes
+    data = [re.sub("\'", "", sent) for sent in data]
+
     words = list()
-    for sentence in sentences:
-        words.append(gensim.utils.simple_preprocess(str(sentence), deacc=True)) # deacc=True removes punctuations
-    return words
+    for sentence in data:
+        words.append(gensim.utils.simple_preprocess(str(sentence), deacc=True))
+    data_words = list(words)
 
-stop_words = stopwords.words('english')
-stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+    lemmatizer = WordNetLemmatizer()
+    data_lemmatized = [[lemmatizer.lemmatize(w) for w in sent if w not in stop_words] for sent in data_words]
 
-df = pandas.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
+    id2word = corpora.Dictionary(data_lemmatized)
+    texts = data_lemmatized
+    corpus = [id2word.doc2bow(text) for text in texts]
 
-# Convert to list
-data = df.content.values.tolist()
-# Remove Emails
-data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
-# Remove new line characters
-data = [re.sub('\s+', ' ', sent) for sent in data]
-# Remove distracting single quotes
-data = [re.sub("\'", "", sent) for sent in data]
+    return gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=id2word, num_topics=20)
 
-data_words = list(sent_to_words(data))
+def save_model(model, dir_name="text_mining_models", file_name="gensim_model"):
+    if not os.path.exists(os.path.join(os.getcwd(), "..", dir_name)):
+        os.makedirs(os.path.join(os.getcwd(), "..", dir_name))
+    model.save(os.path.join(os.getcwd(), "..", dir_name, file_name))
 
-lemmatizer = WordNetLemmatizer()
-data_lemmatized = [[lemmatizer.lemmatize(w) for w in sent] for sent in data_words]
-
-id2word = corpora.Dictionary(data_lemmatized)
-texts = data_lemmatized
-corpus = [id2word.doc2bow(text) for text in texts]
-
-lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
-                                           id2word=id2word,
-                                           num_topics=20)
-
-other_texts = [
-    ['computer', 'time', 'graph'],
-    ['survey', 'response', 'eps'],
-    ['human', 'system', 'computer']
-]
-
-id2word = corpora.Dictionary(other_texts)
-other_corpus = [id2word.doc2bow(text) for text in other_texts]
-unseen_doc = other_corpus[0]
-vector = lda_model[unseen_doc]  # get topic probability distribution for a document
+def load_model(dir_name="text_mining_models", file_name="gensim_model"):
+    path = os.path.join(os.getcwd(), "..", dir_name, file_name)
+    if os.path.isfile(path):
+        return gensim.models.ldamodel.LdaModel.load(path)
+    else:
+        model = create_model()
+        save_model(model, file_name="gensimLDA")
+        return model
--- EmojiTranslator/emoji_mining/sklearn_LDA.py
+++ EmojiTranslator/emoji_mining/sklearn_LDA.py
@@ -1,25 +1,40 @@
+import os
+from joblib import dump, load
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.decomposition import LatentDirichletAllocation
 
-def display_topics(model, feature_names, no_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        print("Topic %d:" % (topic_idx))
-        print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-no_top_words - 1:-1]]))
+class ModelVectorizer(object):
+    def __init__(self, model, vectorizer):
+        self.lda_model = model
+        self.vectorizer = vectorizer
 
-dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
-documents = dataset.data
+def predict_topic(text, model):
+    tf = model.vectorizer.transform([text])
+    topic_probability_scores = model.lda_model.transform(tf)
+    return topic_probability_scores
 
-no_features = 1000
+def create_model(data="20newsgroups"):
+    tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=1000,
+                                    stop_words='english')
+    dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
+    documents = dataset.data
+    tf = tf_vectorizer.fit_transform(documents)
 
-tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
-tf = tf_vectorizer.fit_transform(documents)
-tf_feature_names = tf_vectorizer.get_feature_names()
+    no_topics = 20
+    lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
+    return ModelVectorizer(lda, tf_vectorizer)
 
-no_topics = 20
+def save_model(model, dir_name="text_mining_models", file_name="sklearn_model.joblib"):
+    if not os.path.exists(os.path.join(os.getcwd(), "..", dir_name)):
+        os.makedirs(os.path.join(os.getcwd(), "..", dir_name))
+    dump(model, os.path.join(os.getcwd(), "..", dir_name, file_name))
 
-# Run LDA
-lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
-
-display_topics(lda, tf_feature_names, no_top_words=10)
+def load_model(dir_name="text_mining_models", file_name="sklearn_model.joblib"):
+    path = os.path.join(os.getcwd(), "..", dir_name, file_name)
+    if os.path.isfile(path):
+        return load(path)
+    else:
+        model = create_model("20newsgroups")
+        save_model(model)
+        return model
