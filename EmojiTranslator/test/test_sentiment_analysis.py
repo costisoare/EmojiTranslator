c6deefdb5036f8c79d258442828bd5924def3629
Constantin Soare <costi.soare@yahoo.com>
2019-03-21 13:53:29 +0000

    Minor refactor

--- EmojiTranslator/test/test_sentiment_analysis.py
+++ EmojiTranslator/test/test_sentiment_analysis.py
@@ -2,11 +2,19 @@ import unittest
 from emoji_mining.sentiment_analysis import *
 
 class TestSentimentAnalysis(unittest.TestCase):
-    def test_sent_rating(self):
+    def test_sent_rating_very_positive(self):
         self.assertEqual("very positive", sent_rating(0.75, 0.75)[0])
+
+    def test_sent_rating_positive(self):
         self.assertEqual("positive", sent_rating(0.35, 0.45)[0])
+
+    def test_sent_rating_very_neutral(self):
         self.assertEqual("neutral", sent_rating(0.01, -0.02)[0])
+
+    def test_sent_rating_negative(self):
         self.assertEqual("negative", sent_rating(-0.35, -0.45)[0])
+
+    def test_sent_rating_very_negative(self):
         self.assertEqual("very negative", sent_rating(-0.75, -0.75)[0])
 
     def test_end_to_end(self):
